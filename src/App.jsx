7d664561a4f4c83d2fe974d9015f75de6c456ceb
Arshiya207 <arshiya1383.mk@gmail.com>
2024-10-12 21:41:50 +0330

    finish styling start page

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import StartP from "./components/StartP"
+export default function App(){
+    return (
+        <div className="container"> 
+        <div className="yellow-particle"></div>
+            <StartP/>
+        <div className="violet-particle"></div>
+        </div>
+    )
+}
---
+++ src/components/StartP.jsx
@@ -0,0 +1,9 @@
+export default function StartP(){
+    return (
+        <div className="start-container">
+            <h1 className="start__title">Quizzical</h1>
+            <p className="start__description">let's take a quiz and test your knowledge</p>
+            <button className="large__btn">Start Quiz</button>
+        </div>
+    )
+}
